12d8a313169e55c8b6785404f2cc4e97e9a237fc
Christian Gabrisch <github@cgabrisch.de>
2023-04-03 22:41:19 +0200

    Fix fetching Github user JSON

--- src/observables.js
+++ src/observables.js
@@ -1,21 +1,17 @@
-import { of, tap, map, catchError } from 'rxjs';
+import { of, tap, mergeMap } from 'rxjs';
 import { ajax } from 'rxjs/ajax';
 
 export function testObservables() {
   of('cgabrisch').pipe(
     tap(x => console.log('About to fetch Github user : %O', x)),
-    map(x => fetchGithubUser(x)),
-    tap(x => console.log('Tap 2: %O', x))
-  ).subscribe(x => console.log('Subscription: %O', x));
+    mergeMap(x => githubUserRequest(x)),
+    tap(x => console.log('Fetched user: %O', x.login ))
+  ).subscribe({
+      next: x => console.log('Github user JSON: %O', JSON.stringify(x)),
+      error: error => console.log('Caught error when fetching Github user: ', error)
+  });
 }
 
-function fetchGithubUser(user) {
-  return ajax(`http://api.github.com/users/${user}`).pipe(
-      map(userResponse => console.log('users: ', userResponse)),
-      catchError(error => {
-        console.log('error: ', error);
-        return of(error);
-      })
-    )
+function githubUserRequest(user) {
+  return ajax.getJSON(`http://api.github.com/users/${user}`);
 }
-     
