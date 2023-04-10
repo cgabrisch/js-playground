ca70b47a0cb17008dbb7b7bca05fbd08249e34b3
Christian Gabrisch <github@cgabrisch.de>
2023-04-10 20:20:27 +0200

    Make some class features private

--- src/ticker.js
+++ src/ticker.js
@@ -1,29 +1,32 @@
 class TickerComponent {
+  #updateInterval;
+  #intervalId;
+
   constructor(updateInterval) {
     this.element = document.createElement('div');
     this.element.innerHTML = 'Ticker not started';
-    this.updateInterval = updateInterval;
+    this.#updateInterval = updateInterval;
   }
   
   startTicking() {
-    if (!!this.intervalId) {
+    if (!!this.#intervalId) {
       return;
     }
     
     this.element.innerHTML = 'Ticker active';
-    this.intervalId = setInterval(() => {
+    this.#intervalId = setInterval(() => {
       this.element.innerHTML = new Date().toLocaleTimeString();
-    }, this.updateInterval);
+    }, this.#updateInterval);
   }
   
   stopTicking() {
-    if (!this.intervalId) {
+    if (!this.#intervalId) {
       return;
     }
     
-    clearInterval(this.intervalId);
+    clearInterval(this.#intervalId);
     this.element.innerHTML = 'Ticker inactive';
-    delete this.intervalId;
+    this.#intervalId = undefined;
   }
 }
 
@@ -34,17 +37,21 @@ const TickerModes = {
 };
 
 class TickerComponentController {
+  #tickerComponent;
+  #intervalSec;
+  #togglingTimeoutId;
+
   constructor(tickerComponent) {
-    this.tickerComponent = tickerComponent;
-    this.intervalSec = 15;
+    this.#tickerComponent = tickerComponent;
+    this.#intervalSec = 15;
 
     const intervalInput = document.createElement('input');
     intervalInput.setAttribute('type', 'number');
     intervalInput.setAttribute('min', 1);
     intervalInput.setAttribute('max', 30);
-    intervalInput.setAttribute('value', this.intervalSec);
+    intervalInput.setAttribute('value', this.#intervalSec);
     intervalInput.onchange = () => {
-      this.intervalSec = this._getSanitizedIntValue(intervalInput, this.intervalSec);
+      this.#intervalSec = this.#getSanitizedIntValue(intervalInput, this.intervalSec);
     };
 
     const intervalLabel = document.createElement('label');
@@ -87,49 +94,49 @@ class TickerComponentController {
   set tickerMode(tickerMode) {
     switch (tickerMode) {
       case TickerModes.active: {
-        this._stopToggling();
-        this.tickerComponent.startTicking();
+        this.#stopToggling();
+        this.#tickerComponent.startTicking();
         break;
       }
       case TickerModes.inactive: {
-        this._stopToggling();
-        this.tickerComponent.stopTicking();
+        this.#stopToggling();
+        this.#tickerComponent.stopTicking();
         break;
       }
       case TickerModes.toggling: {
-        this._startToggling();
+        this.#startToggling();
         break;
       }
     }
   }
 
-  _startToggling() {
-    if (!!this._togglingTimeoutId) {
+  #startToggling() {
+    if (!!this.#togglingTimeoutId) {
       return;
     }
 
     const that = this;
     function deactivateTicker() {
-      that.tickerComponent.stopTicking();
-      that._togglingTimeoutId = setTimeout(() => activateTicker(), that.intervalSec * 1000);
+      that.#tickerComponent.stopTicking();
+      that.#togglingTimeoutId = setTimeout(() => activateTicker(), that.#intervalSec * 1000);
     }
   
     function activateTicker() {
-      that.tickerComponent.startTicking();
-      that._togglingTimeoutId = setTimeout(() => deactivateTicker(), that.intervalSec * 1000);
+      that.#tickerComponent.startTicking();
+      that.#togglingTimeoutId = setTimeout(() => deactivateTicker(), that.#intervalSec * 1000);
     }
   
     activateTicker();
   }
 
-  _stopToggling() {
-    if (!!this._togglingTimeoutId) {
-      clearTimeout(this._togglingTimeoutId);
-      delete this._togglingTimeoutId;
+  #stopToggling() {
+    if (!!this.#togglingTimeoutId) {
+      clearTimeout(this.#togglingTimeoutId);
+      this.#togglingTimeoutId = undefined;
     }
   }
 
-  _getSanitizedIntValue(input, fallbackValue) {
+  #getSanitizedIntValue(input, fallbackValue) {
     var value = Number.parseInt(input.value);
     if (!Number.isFinite(value)) {
       value = fallbackValue;
